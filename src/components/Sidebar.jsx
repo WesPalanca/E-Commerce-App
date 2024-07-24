fcb3eb7735db5a8d10ad17c5f3d67df021b8344f
WesPalanca <wespalanca@gmail.com>
2024-07-24 16:27:40 -0400

    added sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+const Sidebar = () =>{
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const toggleSidebar = () =>{
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div className="container">
+            <button className={`toggle-sidebar ${isOpen ? 'open' : 'close'}`} onClick={toggleSidebar}>{isOpen ? "x" : ">"} </button>
+            {isOpen &&<div className="sidebar">
+                <div className="sidebar-content">
+                    <Link to={'/Profile'}>Profile</Link>
+                    <Link to={'/Cart'}>Cart</Link>
+                    <Link to={'/WishList'}>WishList</Link>
+
+                </div>
+            </div>}
+
+        </div>
+    
+
+    )
+}
+
+
+
+
+
+export default Sidebar;
