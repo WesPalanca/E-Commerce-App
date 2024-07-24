2a62cf6278b2b2a8f76fb3087325f867f796ec7c
WesPalanca <wespalanca@gmail.com>
2024-07-24 16:29:19 -0400

    added wishlist

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Home from "./Pages/Home.jsx";
 import Shop from "./Pages/Shop.jsx";
 import Item from "./Pages/Item.jsx";
+import Wishlist from "./Pages/Wishlist.jsx";
 
 
 const App = () =>{
@@ -13,6 +14,7 @@ const App = () =>{
           <Route path='/' element={<Home />} />
           <Route path='/Shop' element={<Shop /> } />
           <Route path='/Shop/Item/:id/:productName' element={<Item /> } />
+          <Route path='/Wishlist' element={<Wishlist />} />
 
       </Routes>
   </Router>
