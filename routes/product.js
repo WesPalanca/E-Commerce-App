da37503235f4692d8b7ff07115494b65c7fc65b4
WesPalanca <wespalanca@gmail.com>
2024-07-24 16:25:34 -0400

    wishlist routes

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,19 @@
-import { getProducts } from '../controllers/product.js';
+import { addToWishList, checkWishlistStatus, getProducts, getUserWishlist, removeFromWishlist, searchProducts } from '../controllers/product.js';
+import { verifyToken } from '../middleware/auth.js';
 import express from 'express';
 const router = express.Router();
 
 router.get("/products", getProducts);
 
+router.get('/products/search', searchProducts);
+
+router.post('/products/wishlist', verifyToken, addToWishList); //prod/products/wishlist
+
+router.get('/products/wishlist/status', verifyToken, checkWishlistStatus);
+
+router.delete('/products/wishlist', verifyToken, removeFromWishlist);
+
+router.get('/products/wishlist', verifyToken, getUserWishlist);
 
 
 
