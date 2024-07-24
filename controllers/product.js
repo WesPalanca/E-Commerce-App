d27ae0248f9dfe6ef219a18272b222aee5b303df
WesPalanca <wespalanca@gmail.com>
2024-07-24 18:24:56 -0400

    cart controllers

--- controllers/product.js
+++ controllers/product.js
@@ -109,3 +109,74 @@ export const getUserWishlist = async(req,res) =>{
         res.status(500).json({success: false, message: "Something went wrong trying to get wishlist"})
     }
 }
+
+export const getUserCart = async(req,res) =>{
+    try{
+        const userId = req.user.userId;
+        const user = await Users.findOne({_id: userId})
+        if (!user){
+            return res.status(401).json({success: false, message: "Something went wrong trying to get user cart"})
+        }
+        const cart = user.cart;
+        res.status(200).json({success:true, cart, message: "got cart"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({success: false, message: "Something went wrong trying to get wishlist"})
+    }
+}
+
+export const addToCart = async(req,res) =>{
+    try{
+        const userId = req.user.userId;
+        const { productId, productName, price, description } = req.body;
+        const productTemplate = {
+            _id: productId,
+            productName: productName,
+            price: price,
+            description: description
+        }
+        const addToUser = await Users.findOneAndUpdate(
+            {_id: userId},
+            {$push: {cart: productTemplate}},
+            {new: true}
+        )
+        res.status(200).json({success: true, message: "added to cart"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({success: false, message: "Something went wrong adding to cart"})
+    }
+}
+
+export const removeFromCart = async(req,res) =>{
+    try{
+        const userId = req.user.userId;
+        const { productId } = req.body;
+        const removeFromUser = await Users.findOneAndUpdate(
+            {_id: userId},
+            {$pull: {cart: {_id: productId}}},
+            {new: true}
+        )
+        res.status(200).json({success: true, message: "Removed from cart"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({success: false, message: "Something went wrong removing from cart"})
+    }
+}
+
+export const checkCartStatus = async (req, res) =>{
+    try{
+        const userId = req.user.userId;
+        const { productId } = req.query;
+        const user = await Users.findOne({_id: userId});
+        const isInCart = user.cart.some(item => item._id.toString() === productId);
+        res.status(200).json({success: true, isInCart, message: "status updated"})
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({success: false, message: "Something went wrong checking cart status"})
+    }
+}
+
