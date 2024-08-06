b7cc17d5cb3536940252f4a1f23379d1faf2260c
WesPalanca <wespalanca@gmail.com>
2024-08-06 17:05:55 -0400

    updated routes

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,13 @@
-import { getProducts, searchProducts } from '../controllers/product.js';
+import { addReviewToProduct, getProduct, getProducts, searchProducts } from '../controllers/product.js';
+import { verifyToken } from '../middleware/auth.js';
 import express from 'express';
 const router = express.Router();
 
 // get and search products
 router.get("/products", getProducts);
 router.get('/products/search', searchProducts);
+router.get('/products/item', getProduct); //api/prod/products/item
+router.post('/item/reviews', verifyToken, addReviewToProduct); //api/prod/item/review
 
 
 
